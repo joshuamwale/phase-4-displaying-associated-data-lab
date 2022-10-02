783a441cf0d5d087f86576e36fad56af221b1c53
Joshua <mwalejoshua89@gmail.com>
2022-10-02 19:21:10 +0300

    Done

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_111617) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_05_10_111617) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.integer "price"
     t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "city"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "items", "users"
